3d3836db196865ae1c68f15dce011d046ea9e3b5
guybracha <guy.bracha@gmail.com>
2023-05-20 11:12:45 +0300

    rearrange codes

--- CLASS/Person.cs
+++ CLASS/Person.cs
@@ -1,22 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace StudentForm.CLASS
 {
     public abstract class Person
     {
-        private string name;
-        private int age;
-        private int id;
+        protected string name;
+        protected int age;
+        protected int id;
+        protected string address;
 
-        public Person(string name, int age, int id)
+        public Person(string name, int age, int id, string address)
         {
             this.name = name;
             this.age = age;
             this.id = id;
+            this.address = address;
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value >= 18 && value <= 120)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public int Id
+        {
+            get { return this.id; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public string Address
+        {
+            get { return this.address; }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
         }
     }
 }
--- CLASS/Student.cs
+++ CLASS/Student.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace StudentForm.CLASS
 {
-    internal class Student
+    public class Student : Person
     {
+        private int student_number;
+        private int status;
+        private int year;
+        private int grade_ave;
+
+        public Student(int student_number, int status, int year, int grade_ave, string name, int age, int id, string address)
+            : base(name, age, id, address)
+        {
+            this.student_number = student_number;
+            this.status = status;
+            this.year = year;
+            this.grade_ave = grade_ave;
+        }
+
+        public int Number
+        {
+            get { return student_number; }
+            set { student_number = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
     }
 }
