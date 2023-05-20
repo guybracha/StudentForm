715fef3c72ca6a0c4f6d3cf32345e386b345a04e
guybracha <guy.bracha@gmail.com>
2023-05-20 10:54:07 +0300

    new classes

---
+++ CLASS/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentForm.CLASS
+{
+    public abstract class Person
+    {
+        private string name;
+        private int age;
+        private int id;
+
+        public Person(string name, int age, int id)
+        {
+            this.name = name;
+            this.age = age;
+            this.id = id;
+        }
+    }
+}
---
+++ CLASS/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentForm.CLASS
+{
+    internal class Student
+    {
+    }
+}
